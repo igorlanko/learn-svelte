57886fca0ebdcc5e3ed7a07957cec96589e3e882
Igor Lanko <mail@igorlanko.com>
2023-02-02 21:00:25 -0500

    adds filtering to the grid component

---
+++ src/Meetups/MeetupFilter.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { createEventDispatcher } from 'svelte'
+	const dispatch = createEventDispatcher()
+
+	import Button from '../UI/Button.svelte'
+
+	let selected = 0
+</script>
+
+<section class="flex mt-4 mx-2">
+	<Button
+		kind={selected === 0 ? 'primary' : 'secondary'}
+		block
+		on:click={() => {
+			selected = 0
+			dispatch('select', 0)
+		}}
+	>
+		View all
+	</Button>
+	<Button
+		kind={selected === 1 ? 'primary' : 'secondary'}
+		block
+		on:click={() => {
+			selected = 1
+			dispatch('select', 1)
+		}}
+	>
+		favorites
+	</Button>
+</section>
--- src/Meetups/MeetupGrid.svelte
+++ src/Meetups/MeetupGrid.svelte
@@ -1,11 +1,27 @@
 <script>
+	import { createEventDispatcher } from 'svelte'
+	const dispatch = createEventDispatcher()
+	import MeetupFilter from './MeetupFilter.svelte'
 	import MeetupItem from './MeetupItem.svelte'
 
 	export let meetups
+
+	// Filter
+	let favsOnly = false
+
+	$: filteredMeetups = favsOnly
+		? meetups.filter((m) => m.isFavorite)
+		: meetups
+
+	function setFilter(event) {
+		favsOnly = event.detail === 1 ? true : false
+	}
 </script>
 
+<MeetupFilter on:select={setFilter} />
+
 <section class="grid grid-cols-2">
-	{#each meetups as meetup}
+	{#each filteredMeetups as meetup}
 		<MeetupItem
 			id={meetup.id}
 			title={meetup.title}
