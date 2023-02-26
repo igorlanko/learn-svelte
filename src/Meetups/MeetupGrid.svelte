dc76cc242279654340f2a655017d2f7b29c4cc7c
Igor Lanko <31314207+igorlanko@users.noreply.github.com>
2023-02-25 19:07:55 -0500

    Loading state while wait (with timer placeholder)

--- src/Meetups/MeetupGrid.svelte
+++ src/Meetups/MeetupGrid.svelte
@@ -1,37 +1,41 @@
 <script>
-	import { createEventDispatcher } from 'svelte'
-	const dispatch = createEventDispatcher()
-	import MeetupFilter from './MeetupFilter.svelte'
-	import MeetupItem from './MeetupItem.svelte'
+    import { createEventDispatcher } from 'svelte'
+    const dispatch = createEventDispatcher()
+    import MeetupFilter from './MeetupFilter.svelte'
+    import MeetupItem from './MeetupItem.svelte'
 
-	export let meetups
+    export let meetups
 
-	// Filter
-	let favsOnly = false
+    // Filter
+    let favsOnly = false
 
-	$: filteredMeetups = favsOnly
-		? meetups.filter((m) => m.isFavorite)
-		: meetups
+    $: filteredMeetups = favsOnly
+        ? meetups.filter((m) => m.isFavorite)
+        : meetups
 
-	function setFilter(event) {
-		favsOnly = event.detail === 1 ? true : false
-	}
+    function setFilter(event) {
+        favsOnly = event.detail === 1 ? true : false
+    }
 </script>
 
 <MeetupFilter on:select={setFilter} />
 
 <section class="grid grid-cols-2">
-	{#each filteredMeetups as meetup (meetup.id)}
-		<MeetupItem
-			id={meetup.id}
-			title={meetup.title}
-			description={meetup.description}
-			imageUrl={meetup.imageUrl}
-			address={meetup.address}
-			email={meetup.email}
-			isFavorite={meetup.isFavorite}
-			on:show-details
-			on:edit-meetup
-		/>
-	{/each}
+    {#if filteredMeetups.length === 0}
+		<p>No meetups yet. Be the first to add one!</p>
+	{:else}
+        {#each filteredMeetups as meetup (meetup.id)}
+            <MeetupItem
+                id={meetup.id}
+                title={meetup.title}
+                description={meetup.description}
+                imageUrl={meetup.imageUrl}
+                address={meetup.address}
+                email={meetup.email}
+                isFavorite={meetup.isFavorite}
+                on:show-details
+                on:edit-meetup
+            />
+        {/each}
+    {/if}
 </section>
